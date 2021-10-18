1af23299ea78dacc0657231f0620cc8dc5797724
Ianlowkey <67780251+Ianlowkey@users.noreply.github.com>
2021-10-18 20:37:24 +0200

    Add files via upload

---
+++ missionVision/missionVission.blade.php
@@ -0,0 +1,52 @@
+<!--
+    Group members:
+        Ian Louw
+        ZintleZotha
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mission and Vision</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="main">
+        <h1 class="heading1">Mission and Vision</h1>
+        <div class="mission">
+            <h1 class="heading2">Our Mission</h1>
+            <p class="text">
+                Our mission is to become an integral part of job creation in South Africa, being involved and associatedwith businesses of the future.
+                Sharing of knowledge and resources to assist local businesses where needed. All in hopes of one day creating an even better South Africa. <br>
+                We aim to:
+                    <ul class="text">
+                        <li>Provide the CPUT community and the public free entrepreneurial skills and mentorship that they can utilize to empower themselves.</li>
+                        <li>Assist with initiatives to promote black economic empowerment by assisting disadvantaged community entrepreneurs in starting small entreprises and
+                            understanding what entrepreneurship entails.</li>
+                    </ul>
+            </p>
+        </div>
+
+        <div class="mission-image">
+            <p class="test">
+                <img src="images/mission-icon.png" alt="mission image">
+            </p>
+        </div>
+
+        <div class="vision-image">
+            <img src="images/vision-icon.png" alt="vision image">
+        </div>
+        <div class="vision">
+            <h1 class="heading2">Our Vision</h1>
+            <p class="text">
+                To assist those in need with their new business venture, and to also inspire those hesitant on taking that leap towards starting their own business.
+                Sharing our knowledge through online lessons and also assisting with the necessities when first starting up in the business world. We hope to provide the tools 
+                needed by those unfortunate so positive changes can be made for a brighter future.
+            </p>
+        </div>
+    </div>
+</body>
+</html>
